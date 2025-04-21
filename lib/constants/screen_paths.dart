e9481ebb6cc85cc3d925e36897dbca0548830208
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-04-21 14:19:49 +0500

    Useful Packages

---
+++ lib/constants/screen_paths.dart
@@ -0,0 +1,30 @@
+///Screen Example Urls
+export 'package:useful_packages/app.dart';
+
+
+
+
+
+
+
+///Widgets
+
+
+
+
+
+
+
+
+///Packages Url
+
+
+
+///Flutter lib
+export 'dart:io';
+export 'package:flutter/material.dart';
+export 'package:flutter/foundation.dart';
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:io';
-
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-import 'app.dart';
+import 'constants/screen_paths.dart';
 
 void main() {
   if (kIsWeb || (!kIsWeb && !Platform.isAndroid)) {
