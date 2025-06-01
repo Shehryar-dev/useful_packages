fe4fa9edaceabebb65e90b8a29d7f30069d05a19
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-06-01 13:09:22 +0500

    Code Update

--- lib/packages/26_animated_icon_button/animated_icon_button.dart
+++ lib/packages/26_animated_icon_button/animated_icon_button.dart
@@ -9,7 +9,7 @@ class AnimatedIconButtonScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Animated Icon Button Example', style: TextStyle(color: Colors.white),),backgroundColor: Colors.indigo.shade900, iconTheme: IconThemeData(color: Colors.white),
+        title: const Text('Animated Icon Button Example', style: TextStyle(color: Colors.white,fontSize: 16),),backgroundColor: Colors.indigo.shade900, iconTheme: IconThemeData(color: Colors.white),
       ),
       body: Center(
         child: AnimatedIconButton(
