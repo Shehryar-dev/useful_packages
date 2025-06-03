693ab64b9bad35878475e5d1594c1dcfca63a1e4
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-06-03 18:12:29 +0500

    Code Update

---
+++ lib/packages/28_line_icons/line_icons.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+
+class LineIconsExampleScreen extends StatelessWidget {
+  const LineIconsExampleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Line Icons Example"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: const [
+          ListTile(
+            leading: Icon(LineIcons.userCircle),
+            title: Text("User Circle"),
+          ),
+          ListTile(
+            leading: Icon(LineIcons.facebookF),
+            title: Text("Facebook Icon"),
+          ),
+          ListTile(
+            leading: Icon(LineIcons.instagram),
+            title: Text("Instagram Icon"),
+          ),
+          ListTile(
+            leading: Icon(LineIcons.search),
+            title: Text("Search Icon"),
+          ),
+          ListTile(
+            leading: Icon(LineIcons.mobilePhone),
+            title: Text("Mobile Phone Icon"),
+          ),
+        ],
+      ),
+    );
+  }
+}
