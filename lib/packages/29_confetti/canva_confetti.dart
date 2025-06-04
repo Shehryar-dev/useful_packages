ae8d2ae619604600b0fd20627e768802fbe8aeba
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-06-04 12:49:27 +0500

    Code Update

--- lib/packages/29_confetti/canva_confetti.dart
+++ lib/packages/29_confetti/canva_confetti.dart
@@ -33,7 +33,7 @@ class _ConfettiExampleScreenState extends State<ConfettiExampleScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Confetti Celebration", style: TextStyle(color: Colors.white), ), iconTheme: IconThemeData(color: Colors.white),backgroundColor: Colors.indigo.shade900,),
-      body: Container(
+      body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
